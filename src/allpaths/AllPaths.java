eafdb1392bff6ea721579c0711fb46b0c6bbd22e
Julia Chen <juliachenjing@gmail.com>
2017-10-12 23:41:05 -0500

    adj

---
+++ src/allpaths/AllPaths.java
@@ -0,0 +1,56 @@
+package allpaths;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class AllPaths<Vertex> {
+
+    private Stack<String> path  = new Stack<String>();   // the current path
+    private Set<String> onPath  = new HashSet<String>();     // the set of vertices on the path
+
+    public AllPaths(Graph G, String s, String t) {
+        enumerate(G, s, t);
+    }
+
+    // use DFS
+    private void enumerate(Graph G, String v, String t) {
+
+        // add node v to current path from s
+        path.push(v);
+        onPath.add(v);
+
+        // found path from s to t - currently prints in reverse order because of stack
+        if (v.equals(t)) 
+            System.out.println(path);
+
+        // consider all neighbors that would continue path with repeating a node
+        else {
+            for (String w : G.adjacentTo(v)) {
+                if (!onPath.contains(w)) enumerate(G, w, t);
+            }
+        }
+
+        // done exploring from v, so remove from path
+        path.pop();
+        onPath.remove(v);
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph();
+        G.addEdge("A", "B");
+        G.addEdge("A", "C");
+        G.addEdge("C", "D");
+        G.addEdge("D", "E");
+        G.addEdge("C", "F");
+        G.addEdge("B", "F");
+        G.addEdge("F", "D");
+        G.addEdge("D", "G");
+        G.addEdge("E", "G");
+        System.out.println(G);
+        new AllPaths(G, "A", "G");
+        System.out.println();
+        new AllPaths(G, "B", "F");
+    }
+
+}
---
+++ src/allpaths/Graph.java
@@ -0,0 +1,19 @@
+package allpaths;
+
+
+public class Graph {
+
+	
+
+	
+	public String [] adjacentTo(String v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addEdge(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
