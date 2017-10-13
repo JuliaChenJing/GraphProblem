c5b6fac563c3b0647ea7fc19ed4c4c2f8c633df3
Julia Chen <juliachenjing@gmail.com>
2017-10-12 20:32:38 -0500

    first commit

---
+++ src/Graph.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+
+
+class Graph {
+
+
+	Node[] nodes;
+
+	Node[] getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(Node[] nodes) {
+		this.nodes = nodes;
+	}
+
+	public static boolean search(Graph graph, Node from, Node to) {
+
+		LinkedList<Node> returnlist = new LinkedList<Node>(); // operates as
+																// Stack
+
+		// set all nodes in graph Unvisited
+		for (Node u : graph.getNodes()) {
+			u.setState(State.Unvisited);
+		}
+
+		from.setState(State.Visiting);
+		returnlist.add(from);// add the first node to the returnlist
+
+		Node u;
+		while (!returnlist.isEmpty()) {
+			u = returnlist.removeFirst(); // remove the first element of the
+											// linkedlist
+			if (u != null) {
+				for (Node v : u.getAdjacent()) {
+					if (v.getState() == State.Unvisited) {
+						if (v == to) {
+							return true;
+						} else {
+							v.setState(State.Visiting);
+							returnlist.add(v);
+						}
+					}
+				}
+
+				u.setState(State.Visited);
+			}
+		}
+		return false;
+	}
+
+
+}
+
---
+++ src/Node.java
@@ -0,0 +1,23 @@
+
+
+class Node {
+	
+		private State state;
+
+		public Node[] getAdjacent() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public State getState() {
+			// TODO Auto-generated method stub
+			return state;
+		}
+
+		public void setState(State visited) {
+
+			state = visited;
+
+		}
+
+	}
